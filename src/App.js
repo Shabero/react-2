8906e9e189af6241bd0e8dc08f1ee82a27b99d8f
Shabero <tokmokiuca@gmail.com>
2023-11-20 22:28:23 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import React, {useState} from 'react';
-import Button from './Components/Button/Button'
+import React, { useState } from 'react';
+import './index.css';
+
+// Calculator.js
+
+
 const App = () => {
-    const [state,setState] = useState(0)
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [result, setResult] = useState('');
 
-    const handleInscrease = () => {
-        setState(state + 1)
-    }
+    const handleAdd = () => {
+        setResult(parseFloat(num1) + parseFloat(num2));
+    };
 
+    const handleSubtract = () => {
+        setResult(parseFloat(num1) - parseFloat(num2));
+    };
 
-    const handleDecrease = () => {
-        if (state === 0) {
-            setState(state)
-        } else {
-            setState(state - 1)
-        }
-    }
+    const handleMultiply = () => {
+        setResult(parseFloat(num1) * parseFloat(num2));
+    };
 
-  return (
-      <div>
-        <h1>Hello</h1>
-          <button onClick={handleInscrease}>Increase</button>
-          <button onClick={handleDecrease}>Decrease</button>
-          <h2>{state}</h2>
-
-          {/*<Button text={'Shabdan'} />*/}
-          {/*<Button text={'Farukh'} />*/}
-          {/*<Button text={'Temirlan'} />*/}
-          {/*<Button text={'Bob'} />*/}
-      </div>
-  )
+    const handleDivide = () => {
+        setResult(parseFloat(num1) / parseFloat(num2));
+    };
+    return (
+        <div className="calculator-container">
+            <h1>Calculator</h1>
+            <div className="input-container">
+                <input
+                    type="number"
+                    value={num1}
+                    onChange={(e) => setNum1(e.target.value)}
+                />
+                <input
+                    type="number"
+                    value={num2}
+                    onChange={(e) => setNum2(e.target.value)}
+                />
+            </div>
+            <div className="button-container">
+                <button onClick={handleAdd}>+</button>
+                <button onClick={handleSubtract}>-</button>
+                <button onClick={handleMultiply}>*</button>
+                <button onClick={handleDivide}>/</button>
+            </div>
+            <div className="result">Result: {result}</div>
+        </div>
+    );
 }
+
 export default App;
+
